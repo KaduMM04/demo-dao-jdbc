a9420a9a8b074d55ee46f36816c578789bf4da87
Kadu Miranda <kadumendesmiranda@gmail.com>
2024-02-14 15:55:17 -0300

    Seller entity class

--- src/db/DB.java
+++ src/db/DB.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 
 public class DB {
 
-	private static Connection conn = null;
+private static Connection conn = null;
 	
 	public static Connection getConnection() {
 		if (conn == null) {
@@ -31,14 +31,15 @@ public class DB {
 		if (conn != null) {
 			try {
 				conn.close();
-			} catch (SQLException e) {
+			}
+			catch(SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 	
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
@@ -57,7 +58,7 @@ public class DB {
 			}
 		}
 	}
-
+	
 	public static void closeResultSet(ResultSet rs) {
 		if (rs != null) {
 			try {
@@ -67,4 +68,5 @@ public class DB {
 			}
 		}
 	}
+
 }
---
+++ src/model/entites/Seller.java
@@ -0,0 +1,101 @@
+package model.entites;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String emali;
+	private Date birthDate;
+	private Double baseSalary;
+	
+	private Department department;
+	
+	public Seller() {
+	}
+
+	public Seller(Integer id, String name, String emali, Date birthDate, Double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.emali = emali;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmali() {
+		return emali;
+	}
+
+	public void setEmali(String emali) {
+		this.emali = emali;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", emali=" + emali + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+}
